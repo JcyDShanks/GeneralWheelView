48d6273cf0e48c5199ef6dd4e93f3af150f2cbf6
姜 春宇 <>
2020-03-23 17:14:01 +0800

    Initial commit

---
+++ app/src/main/java/com/chunyu/generalwheelview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chunyu.generalwheelview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
